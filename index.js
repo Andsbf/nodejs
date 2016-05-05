4ebc6f1aa74c2e195241be17afdbc04b0fbc371a
Anderson Saunders <andsbf@gmail.com>
2016-05-04 20:00:33 -0700

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var app = express()
+
+var fs = require('fs')
+var path = require('path')
+var _ = require('lodash')
+var engines = require('consolidate')
+var favicon = require('serve-favicon');
+
+app.use("/public", express.static(path.join(__dirname, 'public')));
+
+app.set('views', './views')
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+app
+  .get('/', function (req, res) {
+    res.redirect('/perfect');
+  })
+  .get('/favicon.ico', function (req, res) {
+    res.end()
+  })
+  .get('/jadson', function (req, res) {
+    res.render('jadson/index.html');
+  })
+  .get('/jadson/animation', function (req, res) {
+    res.render('jadson/animation.html');
+  })
+  .get('/perfect', function (req, res) {
+    res.render('perfect/index.html');
+  })
+  .get('/perfect/alpha', function (req, res) {
+    res.render('perfect/alpha.html');
+  });
+
+var server = app.listen(3000, function () {
+  console.log('Server running at http://localhost:' + server.address().port);
+});
